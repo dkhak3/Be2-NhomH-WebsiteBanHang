e7d6c91e564f97bebac42c2920d8eea0788f2bb9
KHA  DUY <nhokkha345@gmail.com>
2023-05-28 08:25:34 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomAuthAdminController;
+use App\Http\Controllers\ViewAdminController;
 
 /*
 |--------------------------------------------------------------------------
